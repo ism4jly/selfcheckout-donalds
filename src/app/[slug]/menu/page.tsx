1419d14698303c7147fd7cdf5c957a5a5a2a52c4
ism4jly <ismaelpaulofreitas@gmail.com>
2025-10-06 16:00:16 -0300

    feat: add restaurant menu categories selection and display

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
+import RestaurantCategories from "./components/categories";
 import RestaurantHeader from "./components/header";
 
 interface RestaurantMenuPageProps {
@@ -24,6 +25,11 @@ const RestaurantMenuPage = async ({
   }
   const restaurant = await db.restaurant.findUnique({
     where: { slug },
+    include: {
+      menuCategories: {
+        include: { products: true },
+      },
+    },
   });
   if (!restaurant) {
     return notFound();
@@ -32,6 +38,7 @@ const RestaurantMenuPage = async ({
   return (
     <div>
       <RestaurantHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant} />
     </div>
   );
 };
